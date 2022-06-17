185d722ad5d75ae0df9ac0345f0e4f8372943a79
unknown <vicunite2@gmail.com>
2022-06-17 17:35:44 +0530

    Swagg

--- server.js
+++ server.js
@@ -2,9 +2,34 @@ const express = require('express')
 const app = express()
 const user = require('./Api/user')
 const common = require('./Api/common')
- 
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express') 
 const port = process.env.PORT || 3000
 
+const swaggerOption={
+  definition:{
+    openapi:'3.0.0',
+    info:{
+      title:'NJSYEVV',
+      version:'1.0.0.0',
+      description:'We are under developement',
+      contact:{
+        name:'Victor'
+      },
+      servers:[
+        {
+          url:'http://localhost:3000/'
+        }
+      ]
+    }
+  },
+  apis:['server.js']
+}
+
+const swaggerDocs=swaggerJsDoc(swaggerOption);
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs))
+
+// http://localhost:3000/api-docs/
 
 app.use(express.json()),
 app.use(express.urlencoded({extended: false})); 
