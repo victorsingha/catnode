fd70722cefa121dd9ba8ce7dd238f7c2bfc222d2
unknown <vicunite2@gmail.com>
2022-06-17 15:30:52 +0530

    cl

--- Services/jwt.js
+++ Services/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const key = process.env.TOKEN_KEY || "4nbiu$%$2c67r7#GH@!gr2^%FGVC#RT@S@"
 
 function generateAccessToken(obj) {
-    return jwt.sign(obj, key, { expiresIn: '1800s' });
+    return jwt.sign(obj, key, { expiresIn: '6400s' });
   }
 
 
@@ -11,11 +11,10 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
   
-    if (token == null) return res.sendStatus(401)
+    if (token == null) return res.status(401).send({StatusCode:'F',StatusMessage:"Unauthorized"});
   
     jwt.verify(token, key, (err,obj) => {
-    //   console.log(err)
-      if (err) return res.sendStatus(403)
+      if (err) return res.status(403).send({StatusCode:'F',StatusMessage:"Forbidden"});
       req=obj
       next()
     })
